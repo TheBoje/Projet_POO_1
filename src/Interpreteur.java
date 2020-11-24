610adac14dbaa38063210150501a8d394444db58
Louis Leenart <louis.leenar@gmail.com>
2020-11-24 15:09:16 +0100

    ajout get player

--- src/Interpreteur.java
+++ src/Interpreteur.java
@@ -1,13 +1,18 @@
+import java.util.Scanner;
+
 public class Interpreteur {
 
 	private GameManager gameManager;
+	private Scanner scanner;
 
 	public void read() {
+		String input = scanner.nextLine();
 		// TODO - implement Interpreteur.read
 		throw new UnsupportedOperationException();
 	}
 
 	public Interpreteur() {
+		scanner = new Scanner(System.in);
 		// TODO - implement Interpreteur.Interpreteur
 		throw new UnsupportedOperationException();
 	}
