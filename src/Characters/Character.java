ba9486daeaa52ab3e4fac4761159bba5e882ade3
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-24 14:49:49 +0100

    Oui

--- src/Characters/Character.java
+++ src/Characters/Character.java
@@ -3,44 +3,48 @@ package Characters;
 import Tiles.*;
 
 import java.util.*;
+
 import Objects.*;
 
-public abstract class Character {
+public abstract class Character
+{
 
-	private Tile tile;
-	private Collection<Item> items;
-	private static int DEFAULT_HP = 10;
-	private String name;
-	private int hp;
-	private List<String> speeches;
+    private Tile tile;
+    private Collection<Item> items;
+    private static int DEFAULT_HP = 10;
+    private String name;
+    private int hp;
+    private List<String> speeches;
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName()
+    {
+        return this.name;
+    }
 
-	public abstract void print();
+    public abstract void print();
 
-	public boolean isAlive() {
-		// TODO - implement Character.isAlive
-		throw new UnsupportedOperationException();
-	}
+    public boolean isAlive()
+    {
+        // TODO - implement Character.isAlive
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param cible
-	 */
-	public void attack(Character cible) {
-		// TODO - implement Character.attack
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param cible
+     */
+    public void attack(Character cible)
+    {
+        // TODO - implement Character.attack
+        throw new UnsupportedOperationException();
+    }
 
-	/**
-	 * 
-	 * @param amount
-	 */
-	public void takeDamage(int amount) {
-		// TODO - implement Character.takeDamage
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * @param amount
+     */
+    public void takeDamage(int amount)
+    {
+        // TODO - implement Character.takeDamage
+        throw new UnsupportedOperationException();
+    }
 
 }
