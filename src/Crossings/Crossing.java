df12f4d2aaed8aa05ebc20e5c48c296a3c1a9d58
Louis Leenart <louis.leenar@gmail.com>
2020-11-26 14:34:57 +0100

    code clean up et ajout go

--- src/Crossings/Crossing.java
+++ src/Crossings/Crossing.java
@@ -2,13 +2,25 @@ package Crossings;
 
 import Tiles.Tile;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public abstract class Crossing
 {
 
-	ArrayList<Tile> tiles = null;
+	private ArrayList<Tile> tiles;
+	private boolean isOpen;
+
+	protected Crossing(boolean isOpen, ArrayList<Tile> tiles)
+	{
+		this.isOpen = isOpen;
+		this.tiles = tiles;
+	}
+
+	public boolean isOpen()
+	{
+		return this.isOpen;
+	}
 
 	public void setTiles(ArrayList<Tile> tiles)
 	{
@@ -16,6 +28,21 @@ public abstract class Crossing
 		for (Tile tile : this.tiles)
 		{
 			tile.addCrossing(this);
+		}
+	}
+
+	public Tile getOtherWay(Tile in) throws Exception
+	{
+		if (this.tiles.size() != 2)
+		{
+			String out = String.format("Number of Tiles linked to this crossing invalid, expected 2, has %d", this.tiles.size());
+			throw new Exception(out);
+		}
+		else
+		{
+			int inIndex = this.tiles.indexOf(in);
+			int outIndex = inIndex ^ 1; // XOR 1 -> flip 1 -> 0 ou 0 -> 1
+			return this.tiles.get(outIndex);
 		}
 	}
 }
--- src/GameManager.java
+++ src/GameManager.java
@@ -6,9 +6,13 @@ import java.util.ArrayList;
 
 public class GameManager {
 
+	/***********************************ATTRIBUTES***********************************/
+
 	private World world;
 	private Interpreteur interpreteur;
 	private Player player;
+
+	/***********************************CONSTRUCTOR***********************************/
 
 	public GameManager() {
 		this.world = new World(4);
@@ -19,42 +23,28 @@ public class GameManager {
 		} catch (Exception e)
 		{
 			System.out.println("Can't find player");
-			//e.printStackTrace();
 		}
 	}
 
-	public void printWorld()
-	{
-		this.world.print();
-	}
+	/***********************************METHODS***********************************/
 
 	/**
-	 * 
+	 *
 	 * @param direction
 	 */
-	public void go(String direction) {
+	public void go(String direction) throws Exception
+	{
 		System.out.format("The player is asked to go to [%s]\n", direction);
-		// TODO - implement GameManager.go
-		//throw new UnsupportedOperationException();
+		this.world.movePlayer(this.player, this.player.getTile().getCrossings().get(Integer.parseInt(direction)));
 	}
-
 	/**
-	 * 
+	 *
 	 * @param to
 	 */
 	public void talk(String to) {
 		System.out.format("The player is asked to talk to [%s]\n", to);
 		// TODO - implement GameManager.talk
 		//throw new UnsupportedOperationException();
-	}
-
-	public void getDirections()
-	{
-		ArrayList<Crossing> playerCrossings = this.player.getTile().getCrossings();
-		for (int i = 0; i < playerCrossings.size(); i++)
-		{
-			System.out.format("\t[%d] %s \n", i, playerCrossings.get(i).getClass().getSimpleName());
-		}
 	}
 
 	public void nextTurn() {
@@ -68,5 +58,35 @@ public class GameManager {
 		// TODO - implement GameManager.nextTurn
 		//throw new UnsupportedOperationException();
 	}
+
+	/***********************************GETTERS***********************************/
+	public void getDirections()
+	{
+		ArrayList<Crossing> playerCrossings = this.player.getTile().getCrossings();
+		for (int i = 0; i < playerCrossings.size(); i++)
+		{
+			System.out.format("\t[%d] %s \n", i, playerCrossings.get(i).getClass().getSimpleName());
+		}
+	}
+
+	/***********************************SETTERS***********************************/
+	public void setPlayer(Player player)
+	{
+		this.player = player;
+	}
+
+	/***********************************DISPLAY***********************************/
+	public void printWorld()
+	{
+		this.world.print();
+	}
+
+
+
+
+
+
+
+
 
 }
