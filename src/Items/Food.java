fff8842eaea38ecd963cd69d679f80ed996ec668
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-11-27 10:17:54 +0100

    implémentation des méthode use (en attente de méthodes du player)

--- src/Items/Food.java
+++ src/Items/Food.java
@@ -1,6 +1,7 @@
 package Items;
 
 import Personnages.Personnage;
+import Personnages.Player;
 
 public class Food extends Item
 {
@@ -15,5 +16,10 @@ public class Food extends Item
 
     public void use(Personnage perso)
     {
+        if(perso instanceof Player)
+        {
+            // TODO appelle la modification de la jauge de bouffe du personnage
+            // perso.fillHunger(this.nutValue);
+        }
     }
 }
