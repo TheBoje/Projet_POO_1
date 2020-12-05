af47c9d8878d16b9c80542eadd09afdd9642cb27
Vincent <vincent.commin@etu.univ-poitiers.fr>
2020-12-05 12:40:14 +0100

    ajout des tests pour les armes

--- Tests/TestsItems/ItemTest.java
+++ Tests/TestsItems/ItemTest.java
@@ -57,27 +57,62 @@ public class ItemTest
         this.throwWeapon = new ThrowWeapon(WEAPON_NAME_3, DAMAGES_3);
 
         this.player = new Player(null, new ArrayList<>(), "Bertrand", new ArrayList<>());
-        this.npc = new NPC(null, new ArrayList<>(), "Benoit", new ArrayList<>());
     }
 
     // Test si les armes à distance perdent des munitions quand on les utilise sur une personne existante
     @Test
     void RangeWeapon1()
     {
+        npc = new NPC(null, new ArrayList<>(), "Benoit", new ArrayList<>());
+
         // On vérifie que on à bien une balle en moins et que le npc à perdu des points de vie
         int ammos = this.rangeWeapon1.getAmmunitions();
-        int health = this.npc.getHp();
-        this.rangeWeapon1.use(this.npc);
+        int health = npc.getHp();
+        this.rangeWeapon1.use(npc);
         ammos--;
         assertEquals(ammos, this.rangeWeapon1.getAmmunitions());
-        assertNotEquals(health, this.npc.getHp());
+        assertNotEquals(health, npc.getHp());
 
 
         // On vérifie que quand l'arme n'a plus de balles, on ne tire pas et ne blesse pas le npc
         health = this.npc.getHp();
-        this.rangeWeapon2.use(this.npc);
+        this.rangeWeapon2.use(npc);
         assertEquals(RangeWeapon.DEFAULT_AMMOS, this.rangeWeapon2.getAmmunitions());
-        assertEquals(health, this.npc.getHp());
+        assertEquals(health, npc.getHp());
+    }
+
+    // Test du comportement des armes à distance quand on les utilise sur une personne inexistante
+    @Test
+    void RangeWeapon2()
+    {
+        int ammos = this.rangeWeapon1.getAmmunitions();
+        this.rangeWeapon1.use(null);
+        assertEquals(ammos, this.rangeWeapon1.getAmmunitions());
+
+        this.rangeWeapon2.use(null);
+        assertEquals(RangeWeapon.DEFAULT_AMMOS, this.rangeWeapon1.getAmmunitions());
+    }
+
+    @Test
+    void MeleeWeapon()
+    {
+        npc = new NPC(null, new ArrayList<>(), "Benoit", new ArrayList<>());
+
+        int hp = npc.getHp();
+        this.meleeWeapon.use(npc);
+        assertNotEquals(hp, npc.getHp());
+    }
+
+    @Test
+    void ThrowWeapon()
+    {
+        npc = new NPC(null, new ArrayList<>(), "Benoit", new ArrayList<>());
+
+        int hp = npc.getHp();
+        assertFalse(this.throwWeapon.getHasBeenLaunched());
+        this.throwWeapon.use(npc);
+        assertNotEquals(hp, npc.getHp());
+        assertTrue(this.throwWeapon.getHasBeenLaunched());
     }
 
 }
