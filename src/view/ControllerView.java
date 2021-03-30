5b92e2d8ad93cabeea50b1842711c4a9e034d543
Vincent <vincent.commin@etu.univ-poitiers.fr>
2021-03-30 15:55:13 +0200

    Ajout des fonctions handler des boutons

---
+++ src/view/ControllerView.java
@@ -0,0 +1,54 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.TextFlow;
+
+public class ControllerView
+{
+    @FXML
+    Pane root;
+
+    @FXML
+    Button btnNorth, btnEast, btnSouth, btnWest;
+
+    @FXML
+    TextFlow informations;
+
+    @FXML
+    ImageView gameBoardImage;
+
+    @FXML
+    ListView contextList;
+
+    @FXML
+    GridPane actions;
+
+    @FXML
+    public void handleBtnNorth()
+    {
+
+    }
+
+    @FXML
+    public void handleBtnEast()
+    {
+
+    }
+
+    @FXML
+    public void handleBtnSouth()
+    {
+
+    }
+
+    @FXML
+    public void handleBtnWest()
+    {
+
+    }
+}
